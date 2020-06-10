c7b6cd063348cb2347135ea92f753ca08a597ed7
Gabriel <gabriel.luizvieira01@gmail.com>
2020-06-10 15:28:11 -0300

    Updates

---
+++ precache-manifest.e52b15abc1c61675630542b7b161d231.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3bd414a5895ae970ca490a34094914",
+    "url": "/index.html"
+  },
+  {
+    "revision": "371a28a7dfa9a595a594",
+    "url": "/static/css/main.b9d67525.chunk.css"
+  },
+  {
+    "revision": "90048b8fb712338f4b00",
+    "url": "/static/js/2.f68fcf6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f68fcf6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "371a28a7dfa9a595a594",
+    "url": "/static/js/main.3a446677.chunk.js"
+  },
+  {
+    "revision": "e4a31ac3c43664c6f6b1",
+    "url": "/static/js/runtime-main.7ed81af2.js"
+  }
+]);
